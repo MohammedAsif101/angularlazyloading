a3c4865557b1acd8d3c28a9e716ac5b90a37ed66
asif <ma0632787@gmail.com>
2023-06-18 23:53:03 +0400

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,30 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactComponent } from './contact/contact.component';
 
-const routes: Routes = [];
+import { AboutComponent } from './about/about.component';
+import { ServicesComponent } from './services/services.component';
+import { HomeComponent } from './home/home.component';
+
+
+const routes: Routes = [
+ 
+  {path:'', component: HomeComponent,
+  loadChildren:() =>import('./home/home.module').then(x=>x.HomeModule)},
+
+  {path:'contact', component: ContactComponent,
+
+  loadChildren:() =>import('./contact/contact.module').then(x=>x.ContactModule)},
+
+  {path:'about', component:AboutComponent,
+  
+  loadChildren:() =>import('./about/about.module').then(x=>x.AboutModule)},
+
+
+  {path:'services', component: ServicesComponent,
+
+  loadChildren:() =>import('./services/services.module').then(x=>x.ServicesModule)}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
